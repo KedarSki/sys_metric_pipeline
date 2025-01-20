5db642f32856978623bac3c21899ba95b84ede2b
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2025-01-20 23:26:13 +0100

    20.01.205 modify Basemodel from pydantic

--- src/api/fast_api.py
+++ src/api/fast_api.py
@@ -1,34 +1,31 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 from typing import List, Union
 import pykx as kx
-
-class MetricValue(BaseModel):
-    metric_type: str  # e.g., 'cpu', 'ram', 'disk'
-    value: Union[int, float]  # Metric value
+import json
+import os
 
 class MetricData(BaseModel):
     instance_id: str
-    values: List[MetricValue]
-    timestamp: float
+    cpu_core: int
+    cpu_mode: str
+    cpu_time_usage: float
+    ram_usage: int
+    disk_device: str
+    disk_usage: float
+    date: float
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="src/app/static"), name="static")
 
 @app.post("/metrics")
 async def receive_metrics(metric_data: MetricData):
-    try:
-        for metric in metric_data.values:
-            if metric.metric_type == "cpu":
-                # Example of what you might do with the CPU data
-                kx.q(f"insert into cpu_usage (`{metric_data.instance_id}`, {metric.value}, {metric_data.timestamp})")
-            elif metric.metric_type == "ram":
-                # Handle RAM data
-                kx.q(f"insert into ram_usage (`{metric_data.instance_id}`, {metric.value}, {metric_data.timestamp})")
-            elif metric.metric_type == "disk":
-                # Handle Disk data
-                kx.q(f"insert into disk_usage (`{metric_data.instance_id}`, {metric.value}, {metric_data.timestamp})")
-        return {"status": "Data processed successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    with kx.SyncQConnection("localhost", 5000) as conn:
+        print(conn("test").py())
+    return print(metric_data)
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return FileResponse('src/app/templates/index.html')
