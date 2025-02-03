7cf1a0ba5930664a0e37156e9c5abc72876dda25
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2025-02-03 21:30:12 +0100

    03.02.2025 get method returns json files to the swagger

--- src/api/fast_api.py
+++ src/api/fast_api.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
+from fastapi import FastAPI, HTTPException, Query, Response
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-import json as json
+import json
 import pykx as kx
+
 
 class MetricData(BaseModel):
     instance_id: str
@@ -15,23 +15,25 @@ class MetricData(BaseModel):
     disk_usage: float
     timestamp: float
 
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="src/app/static"), name="static")
 
 try:
     conn = kx.QConnection(host="172.22.170.205", port=5000)
 except Exception as e:
-    print(f"Exception error{e}")
+    print(f"Exception error: {e}")
 
 conn(
     """
     cpu:([] instance_id:`symbol$(); cpu_core:`int$(); cpu_mode:`symbol$(); cpu_time_usage:`float$(); timestamp:`float$());
     disk:([] instance_id:`symbol$(); disk_device:`symbol$(); disk_usage:`float$(); timestamp:`float$());
     ram:([] instance_id:`symbol$(); ram_usage:`int$(); timestamp:`float$());
-    .u.upd:{[table; data] table insert data}
-    
+    .u.upd:{[table; data] table insert data};
+
     """
 )
+
 
 @app.post("/metrics")
 async def receive_metrics(metric_data: MetricData):
@@ -42,63 +44,50 @@ async def receive_metrics(metric_data: MetricData):
                 "cpu_core": [metric_data.cpu_core],
                 "cpu_mode": [kx.SymbolAtom(metric_data.cpu_mode)],
                 "cpu_time_usage": [metric_data.cpu_time_usage],
-                "timestamp": [metric_data.timestamp]
+                "timestamp": [metric_data.timestamp],
             }
         )
-        conn('.u.upd', 'cpu', cpu_list)
+        conn(".u.upd", "cpu", cpu_list)
 
         disk_list = kx.Table(
             data={
                 "instance_id": [kx.SymbolAtom(metric_data.instance_id)],
                 "disk_device": [kx.SymbolAtom(metric_data.disk_device)],
                 "disk_usage": [metric_data.disk_usage],
-                "timestamp": [metric_data.timestamp]
+                "timestamp": [metric_data.timestamp],
             }
         )
-        conn('.u.upd', 'disk', disk_list)
+        conn(".u.upd", "disk", disk_list)
 
         ram_list = kx.Table(
             data={
                 "instance_id": [kx.SymbolAtom(metric_data.instance_id)],
                 "ram_usage": [metric_data.ram_usage],
-                "timestamp": [metric_data.timestamp]
+                "timestamp": [metric_data.timestamp],
             }
         )
-        conn('.u.upd', 'ram', ram_list)
-        print(cpu_list,ram_list,disk_list)
+        conn(".u.upd", "ram", ram_list)
+
         return {"status": "success", "message": "Data stored successfully"}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error storing data: {e}")
 
+
 @app.get("/metrics")
-async def provide_metrics():
+async def provide_metrics(instance_id: str = Query(..., description="Filter by instance ID")):
+
     try:
-        # Retrieve data from KDB+
-        conn(
-            """    
-            jsonstr: "J" sv string each cpu
-            jsonarr: "[" , (jsonstr sv ",") , "]" 
-            0:("/mnt/c/git/sys_metric_pipeline/src/data/cpu.json") enlist jsonarr;
+        formatted_instance_id = instance_id.strip().replace('"', "")
+        cpu_data = conn(f"cpu_json:.j.j select from cpu where instance_id=`{formatted_instance_id}; cpu_json")
+        disk_data = conn(f"disk_json:.j.j select from disk where instance_id=`{formatted_instance_id}; disk_json")
+        ram_data = conn(f"ram_json:.j.j select from ram where instance_id=`{formatted_instance_id}; ram_json")
 
-            """
-        )
-        conn(
-            """    
-            jsonstr: "J" sv string each ram
-            jsonarr: "[" , (jsonstr sv ",") , "]" 
-            0:("/mnt/c/git/sys_metric_pipeline/src/data/ram.json") enlist jsonarr;
+        return {
+            "cpu": json.loads(str(cpu_data)),
+            "disk": json.loads(str(disk_data)),
+            "ram": json.loads(str(ram_data)),
+        }
 
-            """
-        )
-        conn(
-            """    
-            jsonstr: "J" sv string each disk
-            jsonarr: "[" , (jsonstr sv ",") , "]" 
-            0:("/mnt/c/git/sys_metric_pipeline/src/data/disk.json") enlist jsonarr;
-
-            """
-        )
     except Exception as e:
-        print(f"Error providing data: {e}")
-        raise HTTPException(status_code=500, detail=f"Error providing data: {e}")
+        raise HTTPException(status_code=500, detail=f"Error retrieving data: {str(e)}")
--- src/database/kdb/q_scripts/init.q
+++
@@ -1,2 +0,0 @@
-// Define the .u.upd function to allow updates
-.u.upd:{[table; data] table insert data}
